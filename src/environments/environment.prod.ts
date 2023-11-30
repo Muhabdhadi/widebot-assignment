0b7bdac0f0a6c021fcae2493ce818080a2cd54ba
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-30 15:23:39 +0200

    add jsonplaceholder to environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    jsonplaceholder: 'https://jsonplaceholder.typicode.com/'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    jsonplaceholder: 'https://jsonplaceholder.typicode.com/'
+}
