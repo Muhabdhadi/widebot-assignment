3502db011ec85b6d3ef58c6d2e524a0a3868b745
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-30 19:04:10 +0200

    create UI design for user modal

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {AdminService} from "../admin.service";
 import {UserInterface} from "./user.interface";
 import {ToasterService} from "../../shared/toasts/toaster.service";
+import {NgbModal, NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
+import {UserModalComponent} from "../user-modal/user-modal.component";
 
 @Component({
     selector: 'app-users',
@@ -11,7 +13,9 @@ import {ToasterService} from "../../shared/toasts/toaster.service";
 export class UsersComponent implements OnInit {
     users: UserInterface[] = [];
     isLoading = false;
+    userModalComponentRef!: NgbModalRef;
     constructor(private adminService: AdminService,
+                private modalService: NgbModal,
                 private toasterService: ToasterService) {
     }
 
@@ -31,5 +35,15 @@ export class UsersComponent implements OnInit {
                 this.toasterService.show('Error while getting list of users', {className: 'bg-danger text-light'})
             }
         })
+    }
+
+    openUserModal(user: any) {
+        this.userModalComponentRef = this.modalService.open(UserModalComponent, {centered: true});
+        this.userModalComponentRef.componentInstance.user = user;
+        this.userModalComponentRef.componentInstance.cancel.subscribe({
+            next: () => {
+                this.userModalComponentRef.close();
+            }
+        });
     }
 }
