51155f26da7ca7b6e99fe0d1d4ba3393e0b1f364
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-30 15:19:45 +0200

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
