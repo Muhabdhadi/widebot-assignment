67348c2ba55cff2594fedefc62b0d3567412eb51
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-11-30 18:56:31 +0200

    create user modal

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,12 @@ import {AdminRoutingModule} from "./admin-routing.module";
 import { UsersComponent } from './users/users.component';
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../shared/shared.module";
+import { UserModalComponent } from './user-modal/user-modal.component';
 
 @NgModule({
     declarations: [
-    UsersComponent
+    UsersComponent,
+    UserModalComponent
   ],
     imports: [
         CommonModule,
