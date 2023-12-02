eb539ea7132dcab4c28b3dc581c67b4ffbd6813d
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-12-02 12:53:22 +0200

    add ngx-translation configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { HeaderComponent } from './shared/header/header.component';
 import {ToastsComponent} from "./shared/toasts/toasts.component";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http,'./assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [
@@ -18,7 +24,15 @@ import {ToastsComponent} from "./shared/toasts/toasts.component";
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-        NgbModule
+        NgbModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            },
+            defaultLanguage: 'en'
+        })
     ],
     providers: [],
     bootstrap: [AppComponent]
