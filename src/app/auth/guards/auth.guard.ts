a001ff69bcfadecfb782d69fde92b045a12c078a
Muhammad Abdelhadi <Muhabdhadi@gmail.com>
2023-12-02 15:48:57 +0200

    remove unused import

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,5 +1,4 @@
 import { CanActivateFn } from '@angular/router';
-import {Inject} from "@angular/core";
 
 export const authGuard: CanActivateFn = () => {
   const isLogin = JSON.parse(localStorage.getItem('user') as string);
